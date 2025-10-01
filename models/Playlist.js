5193e9f18b495bb6493d3c4e330e152d87bc4e01
NekoSuneVR <Chisdeal2014@hotmail.com>
2025-10-01 19:29:03 +0100

    Update Playlist.js

--- models/Playlist.js
+++ models/Playlist.js
@@ -1,14 +1,25 @@
 module.exports = (sequelize, DataTypes) => {
   const Playlist = sequelize.define('Playlist', {
     name: DataTypes.STRING,
-    cover: DataTypes.STRING
+    cover: DataTypes.STRING,
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   });
 
   Playlist.associate = models => {
+    // Playlist belongs to one user
+    Playlist.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'Owner'
+    });
+
+    // Playlist has many songs
     Playlist.hasMany(models.Song, {
       foreignKey: 'playlistId',
       as: 'Songs',
-      onDelete: 'CASCADE' // optional: delete songs when playlist is deleted
+      onDelete: 'CASCADE'
     });
   };
 
