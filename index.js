611280f8438b802dfa3ecacab6eb5f3014ac5dc5
NekoSuneVR <Chisdeal2014@hotmail.com>
2025-10-01 20:24:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ async function fetchYouTubeStats(youtubeid) {
     };
   } catch (err) {
     console.error(`GET /info failed for ${youtubeid}:`, err);
-    return { views: 0 likes: 0 };
+    return { views: 0, likes: 0 };
   }
 }
 
